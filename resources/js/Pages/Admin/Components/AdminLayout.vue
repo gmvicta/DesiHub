fe3097e4365333383bab9e01ff9ce06febdbc98e
Glen Victa <glenvicta@gmail.com>
2025-02-23 09:29:06 +0800

    first commit

---
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+ 
+       <!-- nav bar start -->
+        <Navbar/>
+       <!-- end -->
+       <!-- side bar start -->
+       <Sidebar/>
+        <!-- end -->
+
+       <slot/>
+
+
+    </div>
+ </template>
+ 
+ <script setup>
+ import { onMounted } from 'vue'
+ import { initFlowbite } from 'flowbite'
+ import Navbar from './Navbar.vue'
+ import Sidebar from './Sidebar.vue'
+ 
+ // initialize components based on data attribute selectors
+ onMounted(() => {
+     initFlowbite();
+ })
+ </script>
