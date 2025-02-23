7a0640bb4866596dc89877d18a95cd9a2de7b9b3
Glen Victa <glenvicta@gmail.com>
2025-02-23 21:16:25 +0800

    9:16

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Product;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -9,6 +10,8 @@ use Inertia\Inertia;
 class ProductController extends Controller
 {
     public function index() {
-        return Inertia::render('Admin/Product/Index');
+        $products = Product::get();
+
+        return Inertia::render('Admin/Product/Index', ['products'=>$products]);
     }
 }
